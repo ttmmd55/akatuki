0bb2995e808bda22129b2cb7611f1f043e27e9a5
ttmmd55 <chenshuting47@gmail.com>
2025-02-25 19:59:04 +0800

    1

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,39 +1,52 @@
 if (document.getElementById('map')){
     mapboxgl.accessToken = 'pk.eyJ1IjoiaG9hbmdoYW5kbiIsImEiOiJjbHp3YnUyc2cwMTl3MmtweWo1MjU0cnQ3In0.kJvlxTy_K1nVvwR8y5O8xA';
-        const map = new mapboxgl.Map({
+    const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/light-v11',
         center: [-0.131532, 51.553267],
         zoom: 12
     });
-    
+
     const geojson = {
         type: 'FeatureCollection',
         features: [
             {
-            type: 'Feature',
-            geometry: {
-                type: 'Point',
-                coordinates: [-0.14166561287145382, 51.59097668916141]
-            }
+                type: 'Feature',
+                geometry: {
+                    type: 'Point',
+                    coordinates: [-0.14166561287145382, 51.59097668916141]
+                },
+                properties: {
+                    title: 'Akatuki MUSWELL HILL'
+                }
             },
             {
                 type: 'Feature',
                 geometry: {
                     type: 'Point',
                     coordinates: [-0.12139841658087784, 51.51555725675136]
+                },
+                properties: {
+                    title: 'Akatuki COVENT GARDEN'
                 }
             }
         ]
-        };
-    
-        for (const feature of geojson.features) {
+    };
+
+    for (const feature of geojson.features) {
         // create a HTML element for each feature
         const el = document.createElement('div');
         el.className = 'marker';
-    
+
+        // add text to each marker
+        const text = document.createElement('span');
+        text.innerHTML = feature.properties.title; 
+        el.appendChild(text);
+
         // make a marker for each feature and add to the map
-        new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
+        new mapboxgl.Marker(el)
+            .setLngLat(feature.geometry.coordinates)
+            .addTo(map);
     }
 }
 
@@ -54,6 +67,9 @@ if (document.getElementById('map1')) {
             geometry: {
                 type: 'Point',
                 coordinates: [-0.14166561287145382, 51.59097668916141]
+            },
+            properties: {
+                title: 'Akatuki MUSWELL HILL'
             }
             }
         ]
@@ -63,6 +79,11 @@ if (document.getElementById('map1')) {
         // create a HTML element for each feature
         const el = document.createElement('div');
         el.className = 'marker';
+
+        // add text to each marker
+        const text = document.createElement('span');
+        text.innerHTML = feature.properties.title; 
+        el.appendChild(text);
     
         // make a marker for each feature and add to the map
         new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map1);
@@ -86,6 +107,9 @@ if (document.getElementById('map2')) {
             geometry: {
                 type: 'Point',
                 coordinates: [-0.12139841658087784, 51.51555725675136]
+            },
+            properties: {
+                title: 'Akatuki COVENT GARDEN'
             }
             }
         ]
@@ -95,6 +119,11 @@ if (document.getElementById('map2')) {
         // create a HTML element for each feature
         const el = document.createElement('div');
         el.className = 'marker';
+
+        // add text to each marker
+        const text = document.createElement('span');
+        text.innerHTML = feature.properties.title; 
+        el.appendChild(text);
     
         // make a marker for each feature and add to the map
         new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map2);
